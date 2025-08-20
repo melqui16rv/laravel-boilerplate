4f0560ada4249f7e90108f9c3edc3d3734ac2242
melqui romero <melquiromero@iMac---melqui.local>
2025-08-20 18:02:09 -0500

    prueba de ejecución en cPanel

---
+++ comandos.cmd
@@ -0,0 +1,5 @@
+# Comando para correr el phpAdmin de mySql
+php -S localhost:8080 -t /opt/homebrew/share/phpmyadmin
+
+# Comando para correr la aplicación en el puerto 8000
+php artisan serve --host=localhost --port=8000
